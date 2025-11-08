f5542a3f1b68b2757f906808f61d82e285a3f8f9
omarsalahmohsen48 <omarsalahmohsen48@gmail.com>
2025-11-08 17:21:45 +0200

    Omar's Homework solution

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,30 @@
-def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+import math
 
+print("enter 3 numbers")
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+# The biggest number
+def find_max_number(num1, num2, num3):
+    max_num = max(num1, num2, num3)
+    return max_num
+
+#The avrige
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    avr_num = (num1 + num2 + num3) / 3
+    return avr_num
+    
+# the Standard Deviation (std)? sorry but i had to look ask chatgpt for this monstrosty of a formila
 
 def find_mean_std(num1, num2, num3):
-    mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
-
+    avr_num = (num1 + num2 + num3) / 3
+    variance = ((num1 - avr_num) ** 2 + (num2 - avr_num) ** 2 + (num3 - avr_num) ** 2) / 3
+    std = math.sqrt(variance)
+    return std
+    
+    
+print(find_max_number(num1, num2, num3))
+print(find_mean(num1, num2, num3))
+print(find_mean_std(num1, num2, num3))
